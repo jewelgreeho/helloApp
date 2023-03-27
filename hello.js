6de702f746209b42609ecbdc62e84ebc3e04f3ea
Md. Jewel Rana <106903153+jewelgreeho@users.noreply.github.com>
2023-03-27 21:13:57 +0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -19,6 +19,10 @@ server.listen(port, hostname, () => {
 // Sure, here's a simple "Hello, World!" program in Node.js:
 
 console.log("Hello, World!");
+console.log("Hello, Bangladesh!");
+console.log("Hello, Bangladesh!");
+console.log("Hello, Bangladesh!");
+console.log("Hello, Bangladesh!");
 
 //############################
 
