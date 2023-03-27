27361c6d28be09d3268b264b308a3fea5b96e2c4
Md. Jewel Rana <106903153+jewelgreeho@users.noreply.github.com>
2023-03-27 21:19:15 +0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -19,3 +19,44 @@ console.log("Hello, World!");
 
 //############################
 
+exports.addCustomerInfo = catchAsyncErrors(async (req, res, next) => {
+
+  const {
+    employmentStatus,
+    nationality,
+    age,
+    monthlyIncome,
+    loanFromOtherBank,
+    monthlyInstallment,
+
+  } = req.body;
+
+
+  try {
+    const eligibility = await Eligibility.create({
+      employmentStatus,
+      nationality,
+      age,
+      monthlyIncome,
+      loanFromOtherBank,
+      monthlyInstallment,
+    });
+
+    res.status(200).json({
+      createDate: new Date().toUTCString(),
+      success: true,
+      status: 200,
+      //customerInfo,
+      message: _response.success,
+      data: { eligibility }
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      message: _response.internalError,
+    });
+
+  }
+
+});
